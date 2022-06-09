44979da563bef715f43dca075672752ca50cae5f
Olajide Oluwatosin <oluwatosinaina424@gmail.com>
2022-06-09 10:44:21 +0100

    Remove unnecessary file

--- week3/test.py
+++
@@ -1,9 +0,0 @@
-import datetime
-from dateutil.relativedelta import relativedelta
-today = datetime.today()
-train_month = today - relativedelta(month=-2)
-val_month = today - relativedelta(month=-1)
-
-train_path = f'./data/fhv_tripdata_{train_month.strftime("%Y-%m")}.parquet'
-val_path = f'./data/fhv_tripdata_{val_month.strftime("%Y-%m")}.parquet'
-print(train_path, val_path)
